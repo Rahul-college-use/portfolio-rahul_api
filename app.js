a9fe7ac2b2d4874dbf6fd3688ffd740d9f1d0b0e
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-04 16:06:47 +0530

    origin2

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 3001;
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 // Middleware
-app.use(cors());
+app.use(cors(),{
+    origin: ["https://portfolio-rahul-mu.vercel.app/","https://post-project-in-portfolio-app.vercel.app/"],
+    credentials: true, 
+});
 app.use(express.json());
 
 // ✅ MongoDB Connection
